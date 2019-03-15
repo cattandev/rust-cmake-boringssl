bb182dfa3c36677f04c07725f939440a3c90a5ed
cattandev <cat.tan.dev@gmail.com>
2019-03-15 06:29:03 +0000

    fix func name mismatch

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::os::raw::c_int;
 #[link(name = "foo", kind = "static")]
 extern "C" {
-    fn aes_128_cbc() -> c_int;
+    fn aes_128_cbc_key_length() -> c_int;
 }
 
 fn main() {
     println!("Rust Code");
 
     unsafe {
-        let a = aes_128_cbc();
+        let a = aes_128_cbc_key_length();
         println!("{:?}", a);
     };
 }
